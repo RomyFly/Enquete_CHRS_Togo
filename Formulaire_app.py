99a649aa7460f666bd6e3790400d374a565a9800
zimkada <zimkada@gmail.com>
2024-08-10 23:16:41 +0100

    Création du formulaire de saisie des réponses

--- Formulaire_app.py
+++ Formulaire_app.py
@@ -34,7 +34,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS REPONSES (
     )
            """)          
 conn.commit()
-conn.close()
+
 
 
 #Création de la fonction d'insertion des données dans la table REPONSES de la base de données
@@ -50,3 +50,37 @@ def insert_data(pathologie, sexe, date_naissance, niveau_etude,type_profession,
                               nbre_heure_sport_hebdo, solitude, relation_conflictuelle,
                               meconnaissance_fr_hta, manque_soutien))
     conn.commit()
+    
+
+
+
+#Création du formaulaire de saisie des réponses
+st.header(':rainbow[Questionnaire sur les socioculturels associés à la décompensation de l’hypertension artérielle]:hospital:', anchor='CENTER')
+
+with st.form("Formulaire"):
+    st.subheader(":red[Pathologie]")
+    pathologie = st.selectbox("Type d'hypertension artérielle ",["compensée","Décompensée"])
+    
+    st.subheader(":red[Facteurs sociodémographiques]")
+    sexe = st.radio("Sexe",["Masculin","Féminin"])
+    date_naissance = st.text_input("Date de naissance (dd-mm-yyyy)")
+    niveau_etude = st.selectbox("Niveau d'étude",["Non alphabétisé","Primaire","Secondaire","Universitaire"])
+    
+    st.subheader(":red[Facteurs socioculturels]")
+    type_profession = st.selectbox("Type de profession", ["Physique", "Non physique"])
+    nbre_heure_sport_hebdo = st.number_input("Nombre d'heures hebdomadaires d'activités sportives", min_value = 0.0, step = 0.5)
+    solitude = st.radio("Vivre en solitude", ["Oui", "Non"])
+    relation_conflictuelle = st.radio("Être dans une relation conflictuelle",["Oui","Non"])
+    meconnaissance_fr_hta = st.radio("Méconnaissance des facteurs de risques de l’HTA",["Oui","Non"])
+    manque_soutien = st.radio("Manque de soutien social et économique",["Oui","Non"])
+    
+    submit_button = st.form_submit_button("Soumettre!")
+    
+
+if submit_button:
+    insert_data(pathologie, sexe, date_naissance, niveau_etude,type_profession, 
+                    nbre_heure_sport_hebdo, solitude, relation_conflictuelle, 
+                    meconnaissance_fr_hta, manque_soutien)
+    st.success("Données soumis avec succès!")
+    
+conn.close()  
